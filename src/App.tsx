476f1a5a962df50a44c85b79f046027b06187bec
MiniGT <bendanillo539@gmail.com>
2024-10-15 18:27:38 +0800

    logic for the notes app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import Sidebar from "./components/Sidebar"
+import Editor from "./components/Editor"
+// import { data } from "./data"
+import Split from "react-split"
+import {nanoid} from "nanoid"
+import { NewNoteType } from "./utility/types"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  const [notes, setNotes] = useState<NewNoteType[]>([])
+  const [currentNoteId, setCurrentNoteId] = useState(
+      (notes[0] && notes[0].id) || ""
+  )
+  
+  function createNewNote() {
+    const newNote: NewNoteType = {
+        id: nanoid(),
+        body: "# Type your markdown note's title here"
+    }
+    setNotes(prevNotes => [newNote, ...prevNotes])
+    setCurrentNoteId(newNote.id)
+  }
+  
+  function updateNote(text: string) {
+    setNotes(oldNotes => oldNotes.map(oldNote => {
+    return oldNote.id === currentNoteId
+      ? { ...oldNote, body: text }
+      : oldNote
+    }))
+  }
+  
+  function findCurrentNote() {
+    return notes.find(note => {
+      return note.id === currentNoteId
+    }) || notes[0]
+  }
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <main>
+    {
+      notes.length > 0 
+      ?
+      <Split 
+        sizes={[30, 70]} 
+        direction="horizontal" 
+        className="split"
+      >
+        <Sidebar
+          notes={notes}
+          currentNote={findCurrentNote()}
+          setCurrentNoteId={setCurrentNoteId}
+          newNote={createNewNote}
+        />
+        {
+          currentNoteId && 
+          notes.length > 0 &&
+          <Editor 
+            currentNote={findCurrentNote()} 
+            updateNote={updateNote} 
+          />
+        }
+      </Split>
+      :
+      <div className="no-notes">
+        <h1>You have no notes</h1>
+        <button 
+          className="first-note" 
+          onClick={createNewNote}
+        >
+          Create one now
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    }
+    </main>
   )
 }
-
-export default App
