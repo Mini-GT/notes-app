cb22c1f86190e39979014e24ca7b2626ff65fc17
MiniGT <bendanillo539@gmail.com>
2024-10-20 02:51:44 +0800

    downgrading to react 17

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom"
+import './index.css';
+import App from "./App"
+// downgrading to react 17 because react-mde and react-split libraries isn't supported in react 18
+ReactDOM.render(<App />, document.getElementById("root"))
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+//react 18
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.tsx'
+// import './index.css'
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
