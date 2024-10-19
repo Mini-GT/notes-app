95a83e7859502ab7205596e887a503b95b35cf01
MiniGT <bendanillo539@gmail.com>
2024-10-20 04:08:20 +0800

    exporting db

--- src/backend/firebase.ts
+++ src/backend/firebase.ts
@@ -12,7 +12,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
+export const db = getFirestore(app)
 
 //finds the collection in firabse db in firbase collection 
 //and passing db inside to know which db it is inside the firebase
